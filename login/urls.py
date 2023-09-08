bdc649bc2aa8589fe34a49f506f47b98829223cf
Godha Karthik Reddy <paulatreides@duck.com>
2023-09-08 20:22:31 +0530

    basic authentication app

--- login/urls.py
+++ login/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
+    path("", views.base, name="base"),
+    path("login/", views.index, name="login")
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+def index(request):
+    return render(request, "login/index.html")
+
+
+def base(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, "login/index.html", {
+                "message": "Invalid Username or Password! Try again"
+                })
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+    #else:
+    #   return HttpResponceRedirct(reverse("home"))
